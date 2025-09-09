6edfa537aabf56c03be459cb2401d4f83797c844
Tobias Rojas <tobiasurielrojas2004@gmail.com>
2025-09-09 18:33:09 -0300

    Modificando env

--- frontend/.netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ frontend/.netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_wcarfM8N.mjs';
+import { manifest } from './manifest_Bpf_MwZG.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const serverIslandMap = new Map();;
@@ -28,7 +28,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "7ffcb28f-eb59-4457-a7ed-fef0ccddb3ff"
+    "middlewareSecret": "d0081a3d-af2e-4406-882a-92d1a8473abd"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
