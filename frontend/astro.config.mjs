e593a3ab890a5dd819b0c466126a99804ee74d9a
Tobias Rojas <tobiasurielrojas2004@gmail.com>
2025-09-09 17:23:20 -0300

    preparando deploy de frontend

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,21 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false, 
-  },
+  devToolbar: { enabled: false },
   integrations: [react()],
   vite: {
     resolve: {
-      alias: {
-        '@components': '/src/components',
-        '@styles': '/src/styles',
-        '@utils': '/src/utils'
-      }
+      '@components': '/src/components',
+      '@styles': '/src/styles',
+      '@utils': '/src/utils',
     }
   }
 });
--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,2 +1,2 @@
 export const LIMIT = 20
-export const BASE_API_URL = "http://localhost:3000"
+export const BASE_API_URL = import.meta.env.BASE_API_URL
